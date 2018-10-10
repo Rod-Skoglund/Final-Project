c63e6c25c26a408d2ef70428aa3133902d286492
rschwartz <37844284+randallwschwartz@users.noreply.github.com>
2018-10-10 16:25:27 -0500

    10/10 rws updates to picks and leaderboard pages

--- football/src/utils/API.js
+++ football/src/utils/API.js
@@ -22,15 +22,15 @@ export default {
   getUsers: function() {
     return axios.get("/api/users");
   },
-  // Gets the pick with the given id
+  // Gets the user with the given id
   getUser: function(id) {
     return axios.get("/api/users/" + id);
   },
-  // Deletes the pick with the given id
+  // Deletes the user with the given id
   deleteUser: function(id) {
     return axios.delete("/api/users/" + id);
   },
-  // Saves a pick to the database
+  // Saves a user to the database
   saveUser: function(userData) {
     return axios.post("/api/users", userData);
   }
---
+++ scripts/seedDB.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/dropkick"
+);
+
+const userSeed = [
+  {
+    username: "johndoe",
+    password: "abc",
+    firstName: "john",
+    lastName: "doe",
+    totalPoints: "100",
+    date: new Date(Date.now())
+  },
+  {
+    username: "janedoe",
+    password: "def",
+    firstName: "jane",
+    lastName: "doe",
+    totalPoints: "150",
+    date: new Date(Date.now())
+  }
+
+];
+
+db.User
+  .remove({})
+  .then(() => db.User.collection.insertMany(userSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+
+
+const pickSeed = [
+  {
+    gameId: "1",
+    homeTeam: "Team 1",
+    awayTeam: "Team 2",
+    pick: "Team 1",
+    date: new Date(Date.now())
+  },
+  {
+    gameId: "2",
+    homeTeam: "Team 3",
+    awayTeam: "Team 4",
+    pick: "Team 3",
+    date: new Date(Date.now())
+  }
+
+];
+
+db.Pick
+  .remove({})
+  .then(() => db.Pick.collection.insertMany(pickSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+  
