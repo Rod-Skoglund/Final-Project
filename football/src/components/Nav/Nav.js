0fc9c2909ab2a366352ae677133dd11a82ce333e
RLS <rod.skoglund@gmail.com>
2018-10-25 16:29:08 -0500

    updated colors

--- football/src/components/Nav/Nav.js
+++ football/src/components/Nav/Nav.js
@@ -2,50 +2,50 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Nav = props => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <nav className="navbar navbar-expand-lg navbar-dark bg-success text-white mt-2">
     <Link className="navbar-brand" to="/">
       Drop-Kick Picks
     </Link>
     <div>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toggler text-white" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
-          <li className="nav-item">
+          <li className="nav-item mt-2">
             <Link
-              to="/leaderboard"
-              className={
-                window.location.pathname === "/leaderboard"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
+              to="/leaderboard" className="nav-link text-white"
+              // className={
+              //   window.location.pathname === "/leaderboard"
+              //     ? "nav-link active"
+              //     : "nav-link"
+              // }
             >
               Leaderboard
             </Link>
           </li>
-          <li className="nav-item">
+          <li className="nav-item mt-2">
             <Link
-              to="/picks"
-              className={
-                window.location.pathname === "/picks"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
+              to="/picks" className="nav-link text-white"
+              // className={
+              //   window.location.pathname === "/picks"
+              //     ? "nav-link active"
+              //     : "nav-link"
+              // }
             >
               Weekly Picks
             </Link>
           </li>
-          <li className="nav-item">
+          <li className="nav-item mt-2">
           {/* try anchor - look at react-router for simple authentication */}
             <Link
               // to={{pathname: "/", state: props.isLoggedIn}} 
-              to={{pathname: "/", state: {isLoggedIn: false}}} 
-              className={
-                window.location.pathname === "/" //|| window.location.pathname === "/signin"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
+              to={{pathname: "/", state: {isLoggedIn: false}}} className="nav-link text-white"
+              // className={
+              //   window.location.pathname === "/" //|| window.location.pathname === "/signin"
+              //     ? "nav-link active"
+              //     : "nav-link"
+              // }
             >
               Sign In / Sign Up
             </Link>
